1c732cea8deaf98f93ce9f9ad4e1d1aa53cfe31f
snowflake25 <snowflake_25@naver.com>
2017-03-30 14:53:03 +0900

    oddddo.c  ver.2

--- oddddo.c
+++ oddddo.c
@@ -1,1 +1,24 @@
-#include <stdio.h>#include<stdlib.h>int main(){ int count; int *arr; int i = 0; int num = 1; int sum = 0; int k = 1; scanf("%d", &count); arr = (int *)malloc(sizeof(int)*count); //홀수 찾아서 넣기 while (i < count){  if (num % 2 == 1){   arr[i] = num;   i++;  }  num++; } for (i = 0; i < count; i++){  sum += arr[i] * arr[count - k];  k++; } printf("%d", sum); return 0;}
+#include <stdio.h>#include<stdlib.h>
+int main(){ 
+  int count; 
+  int *arr; 
+  int i = 0; 
+  int num = 1; 
+  int sum = 0; 
+  int k = 1; 
+  
+  scanf("%d", &count); 
+  arr = (int *)malloc(sizeof(int)*count); 
+  //홀수 찾아서 넣기 
+  while (i < count){  
+    if (num % 2 == 1){   
+      arr[i] = num;   
+      i++;  }  num++; 
+  } 
+  for (i = 0; i < count; i++){  
+    sum += arr[i] * arr[count - k];  
+    k++; 
+  } 
+  printf("%d", sum); 
+  return 0;
+}
